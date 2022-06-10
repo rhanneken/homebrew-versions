8dec1268e42c2061ee36a8cbb8456a6545453040
Russell Hanneken <rhanneken@pobox.com>
2022-06-10 12:06:36 -0400

    Create Homebrew formula for ImageMagick 6.9.10-97
    This formula specifically builds and installs version 6.9.10-97
    of ImageMagick. The old homebrew-core formula for that version
    is no longer parsable by recent versions of Homebrew's brew
    command.

---
+++ Formula/imagemagick@6.9.10-97.rb
@@ -0,0 +1,70 @@
+class ImagemagickAT691097 < Formula
+  desc "Tools and libraries to manipulate images in many formats"
+  homepage "https://legacy.imagemagick.org/"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.10-97.tar.xz"
+  sha256 "a7c84103ddf5759eae39fb90fda3803fb32c7739f1cbebcb21d11e052f0ab943"
+  license "ImageMagick"
+  head "https://github.com/imagemagick/imagemagick6.git", tag: "6.9.10-97", revision: "1ec79babb7c8b6996939d30c90fa2b731d2c4f36"
+
+  livecheck do
+    url "https://download.imagemagick.org/ImageMagick/download/"
+    regex(/href=.*?ImageMagick[._-]v?6\.9\.10-97\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+
+  depends_on "freetype"
+  depends_on "ghostscript"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
+
+  skip_clean :la
+
+  def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+
+    args = %W[
+      --enable-osx-universal-binary=no
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-opencl
+      --disable-openmp
+      --enable-shared
+      --enable-static
+      --with-freetype=yes
+      --with-modules
+      --with-webp=yes
+      --with-openjp2
+      --with-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --without-fftw
+      --without-pango
+      --without-x
+      --without-wmf
+    ]
+
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/convert -version")
+    %w[Modules freetype jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
+  end
+end
